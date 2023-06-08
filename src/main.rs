b74909eeea3014cebf34e56b7046e40d13ee9307
web3ten0 <web3ten0@gmail.com>
2023-06-08 22:20:21 +0900

    tokioで非同期を並行に実行

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 mod my_macros;
+use tokio::time::{Duration, sleep};
 
-fn main() {
+async fn count_up_to(id: char, max: i32) {
+    for i in 1..=max {
+        println!("{}:{}", id, i);
+        sleep(Duration::from_millis(5)).await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
     hello!();
     println!("{}", double!(123));
     hoge!(pc, GET);
     pc();
     hoge!(sp, POST);
     sp();
+
+    let tasks = vec![
+        tokio::spawn(count_up_to('a', 10)),
+        tokio::spawn(count_up_to('b', 10)),
+    ];
+
+    // Wait for all tasks to complete.
+    for task in tasks {
+        task.await.unwrap();
+    }
 }
